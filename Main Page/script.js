5250a94b72c8db754bbada7bcf6cbffc2eba6dd8
Alexis Tapia <alexist8888@gmail.com>
2025-03-09 22:36:37 -0700

    test

--- Main Page/script.js
+++ Main Page/script.js
@@ -2,3 +2,6 @@ const sorting_button = document.querySelector(".sorting-button");
 sorting_button.addEventListener("click", (event) => {
     window.location.href = "../Sorting Page/Sorting.html"
 })
+const word = "hello"
+//want to add this comment to github
+//comment
